f68f842af46b15d99ae7e98ea5e9716038b06bf3
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-08 15:14:24 -0700

    added 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - Entry point
+ *Description:'the program's description'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char str1[51];
+
+strcpy(str1, "Programming is like building a multilingual puzzle");
+puts(str1);
+return (0);
+}
