52b218aef2b25e7e9f3571191dc3b81aa1e759e3
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-08 16:29:04 -0700

    added 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  *  *main - Entry point
@@ -8,6 +9,11 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char str1[] = "and that piece of art is useful\" - ";
+char str2[] = "Dora Korpar, 2015-10-19\n";
+
+strcat(str1, str2);
+
+fprintf(stderr, "%s", str1);
 return (1);
 }
