6b2cc296cbd43595cd30ca207155ebcf40a99413
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-12 18:47:11 -0700

    added 5-print_numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  *  main - Entry point
+ *   *  Description:'the program's description'
+ *    *  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch = 0;
+
+	while (ch <= 9)
+	{
+		printf("%d", ch);
+		ch++;
+	}
+
+	printf("\n");
+	return (0);
+}
