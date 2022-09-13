2dcea2f4b1a8148bafecd0268dd28f725bc905ff
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-13 16:47:36 -0700

    added 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  *  _isalpha - Entry point
+ *   *  Description:'the program's description'
+ *    * @c: param
+ *     *  Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= "a" && c <= "z" || c >= "A" && c <= "Z")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
