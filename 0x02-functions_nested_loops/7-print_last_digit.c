658884e24063c59a44e88ba9e955cadce9e9c340
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-13 17:36:04 -0700

    added 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+
+/**
+ *  *  print_last_digit - Entry point
+ *   *  Description:'the program's description'
+ *    *  @n : the character to check
+ *     *  Return: Always 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
