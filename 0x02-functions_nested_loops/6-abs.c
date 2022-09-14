f5c29a62c1d294934a135277123f6f439e52028e
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-13 17:28:42 -0700

    added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  *  _abs - Entry point
+ *   *  Description:'the program's description'
+ *    *  @n : the character to check
+ *     *  Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
