27ef76cd4f7cb525ceb11884c6fea4e34dc790bf
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-14 20:33:48 -0700

    added 100-times_table.c file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+
+/**
+ *  *  print_times_table - Entry point
+ *   *  Description:'the program's description'
+ *    *  @n: The first integer to be added.
+ *      *
+ *       * Return: The result of the addition.
+*/
+
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+
+					_putchar(' ');
+
+				if (prod <= 9)
+
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
