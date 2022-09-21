924c2eb7f3d1cf5fb29001ef404a624e6a2bd24a
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-21 16:15:21 -0700

    added 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  *  mul - Entry point
+ *   *  Description:'the program's description'
+ *    * @a: The character to be checked.
+ *     * @b: The character to be checked.
+ *      * Return: The result of the addition.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
