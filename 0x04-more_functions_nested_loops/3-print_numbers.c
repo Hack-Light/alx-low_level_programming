18594436b5058741d7d6148a2bf3fc79967c11bf
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-21 16:43:22 -0700

    added 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,11 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	num = 0;
+
+	while (num <= 9)
 	{
-		_putchar((num % 10) + '0');
+		_putchar(num + '0');
 	}
 	_putchar('\n');
 }
