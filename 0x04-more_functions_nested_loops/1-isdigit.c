27c6d5820cda61d3005990a1177c2d304e732998
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-21 16:05:00 -0700

    added 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  *  _isdigit - Entry point
+ *   *  Description:'the program's description'
+ *    * @c: The character to be checked.
+ *     * Return: The result of the addition.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
