56e7d73a8cd564dc24e374a0906a2d4b53da2a77
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-09-21 16:25:33 -0700

    added 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  *  print_numbers - Entry point
+ *   *  Description:'the program's description'
+*/
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
