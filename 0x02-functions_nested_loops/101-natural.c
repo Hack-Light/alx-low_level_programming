1eef30c4b773d61fb481c7c5d6d8a6e1f66b309c
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-12 16:18:58 -0700

    added 101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ *  *  main - Entry point
+ *   *  Description:'the program's description'
+ *    * Return: The result of the addition.
+*/
+
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+		i++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
