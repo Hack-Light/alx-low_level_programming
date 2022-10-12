72f400ddf84cdf490d038e8e33d73538fbf40963
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-12 16:41:19 -0700

    added 102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  *  main - Entry point
+ *   *  Description:'the program's description'
+ *    * Return: The result of the addition.
+*/
+
+int main(void)
+{
+
+	int i = 0;
+	unsigned long prev1 = 0;
+	unsigned long prev2 = 1;
+	unsigned long current;
+
+	while (i < 50)
+	{
+		current = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = current;
+		i++;
+
+		printf("%lu, ", current);
+	}
+
+	printf("\n");
+	return (0);
+}
+
