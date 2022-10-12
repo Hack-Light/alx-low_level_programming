7a6de4f4c79bc459fa12fe1393c225db7d52a25d
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-12 16:59:19 -0700

    added 103-fibonacci.c file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *  *  main - Entry point
+ *   *  Description:'the program's description'
+ *    * Return: The result of the addition.
+*/
+
+int main(void)
+{
+	unsigned long i = 0;
+	unsigned long prev1 = 0;
+	unsigned long prev2 = 0;
+	unsigned long current = 0;
+	unsigned long sum = 0;
+
+	while (i < 4000000)
+	{
+		current = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = current;
+		i++;
+
+		if (current % 2 == 0)
+		{
+			sum = current;
+		}
+	}
+
+	printf("%lu", sum);
+	printf("\n");
+	return (0);
+}
