82899bda6a4a9963a0729726a2f8856dc113eb51
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-13 00:18:12 -0700

    added 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description:'the program's description'
+ *   * @void: the number of lines to be drawn
+ *    * Return: The result of the addition.
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && 1 % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		printf(" ");
+		i++;
+	}
+	printf('\n');
+	return (0);
+}
