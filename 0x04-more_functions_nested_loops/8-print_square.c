4ea014f70b3895c210cf9047a4864155ef985c8e
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-12 23:56:02 -0700

    added 8-print_square.c file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ *  *  print_square - Entry point
+ *   * Description:'the program's description'
+ *   * @size: the number of lines to be drawn
+ *    * Return: The result of the addition.
+*/
+
+
+void print_square(int size)
+{
+	if (n > 0)
+	{
+		int col = 1;
+
+		while (col <= size)
+		{
+			int row = 1;
+
+			while (row <= size)
+			{
+				_putchar('#');
+				row++;
+			}
+			col++;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
