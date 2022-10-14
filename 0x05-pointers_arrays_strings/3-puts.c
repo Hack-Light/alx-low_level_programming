454c333352063086b3b9a0e44eb7c5576f679fe7
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-14 04:37:50 -0700

    added 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string followed by a new line
+ * @str: The string to be printed
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	int num = 0;
+
+	while (*(str + num) != '\0')
+	{
+		_putchar(*(str + num));
+		num++;
+	}
+
+	_putchar('\n');
+}
