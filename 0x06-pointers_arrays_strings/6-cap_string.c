48f04ba760d7d124292c1d10c0c6821579b33669
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-10-15 12:12:04 -0700

    added  file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * cap_string - a function that capitalizes evry word of a string
+ * @str: the string to capitalize
+ *
+ * Return: str
+*/
+char *cap_string(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (str[len] >= 'a' && str[len] <= 'z')
+		{
+			if (str[len - 1] == '.' || str[len - 1] == ' ')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '\t' || str[len - 1] == '\n')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == ';' || str[len - 1] == ',')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '!' || str[len - 1] == '?')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '"' || str[len - 1] == '(')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == ')' || str[len - 1] == '{')
+				str[len] = str[len] - 32;
+			if (str[len - 1] == '}')
+				str[len] = str[len] - 32;
+			if (len == 0)
+				str[len] = str[len] - 32;
+			len++;
+		}
+		else
+			len++;
+	}
+	return (str);
+}
